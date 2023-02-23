067eec61c9d5062e6dabfa4d06a5128d465b0821
Andrew Grant <andrew.grant@codecomputerlove.com>
2023-02-23 13:47:57 +0000

    Better variable names

--- 02-functional-typescript-1/target-typescript/src/domain.ts
+++ 02-functional-typescript-1/target-typescript/src/domain.ts
@@ -1,33 +1,33 @@
 import { compose } from "./utility";
 
 export namespace Account {
-    type Current = { readonly kind: 'current', readonly value: number };
-    type Savings = { readonly kind: 'savings', readonly value: number };
+    type Current = { readonly kind: 'current', readonly balance: number };
+    type Savings = { readonly kind: 'savings', readonly balance: number };
     type Account = Current | Savings;
 
     type CalculateInterest = (account: Account) => number;
     type TellMeMyInterest = (account: Account) => string;
 
-    export function createCurrent(value: number): Account {
-        return { kind: 'current', value: value };
+    export function createCurrent(balance: number): Account {
+        return { kind: 'current', balance: balance };
     }
 
-    export function createSavings(value: number): Account {
-        return { kind: 'savings', value: value };
+    export function createSavings(balance: number): Account {
+        return { kind: 'savings', balance: balance };
     }
 
     export const calculateInterest: CalculateInterest =
         (account: Account) => {
             switch (account.kind) {
                 case "current":
-                    return account.value * 0.02;
+                    return account.balance * 0.02;
                 case "savings":
-                    return account.value * 0.05;
+                    return account.balance * 0.05;
             }
         };
 
-    function formatInterest(value: number) {
-        return `You've earned ${value} pence interest`
+    function formatInterest(pence: number) {
+        return `You've earned ${pence} pence interest`
     }
 
     export const tellMeMyInterest: TellMeMyInterest =
